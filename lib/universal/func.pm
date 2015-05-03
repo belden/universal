a8d151403a4e2a5382192a475855c828fe5227db
Belden Lyman <belden.lyman@gmail.com>
2015-05-03 15:51:32 +0000

    first-pass for universal::func
    todo:
    - tidy up call stack
    - act like a pragma

---
+++ lib/universal/func.pm
@@ -0,0 +1,37 @@
+package universal::func;
+use strict;
+use warnings;
+
+sub UNIVERSAL::func {
+  my $orig_class = shift;
+
+  my $function_dispatcher = "$orig_class\::function_dispatcher";
+  (my $putative_file_name = $function_dispatcher) =~ s{::}{/}g; # assumes '/' is your OS's path separator
+
+  if (! $INC{$function_dispatcher}++) {
+    local $@;
+    eval <<EVAL;
+package $function_dispatcher;
+
+sub AUTOLOAD {
+  my \$class = shift;
+
+  my (\$method) = our \$AUTOLOAD =~ m{^.*:(.+)};
+
+  my \$target_class = '$orig_class';
+  my \$subref = UNIVERSAL::can(\$target_class, \$method);
+
+  if (! defined \$subref) {
+    require Carp;
+    Carp::croak("Undefined subroutine &\$target_class\::\$method");
+  }
+
+  \$subref->(\@_);
+}
+EVAL
+    die $@ if $@;
+  }
+  return $function_dispatcher;
+}
+
+1;
