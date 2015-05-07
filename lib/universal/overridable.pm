be865207ba102530425e8e20d93af015f17a460f
Belden Lyman <belden.lyman@gmail.com>
2015-05-07 16:42:58 +0000

    non-overridable CORE functions now trigger compile errors
    Any Perl function that has CORE::prototype of undef is a non-overridable
    function. Well, actually, that's not quite true; that's just what
    'perldoc -f prototype' woudld have you believe.
    
    A full list of perl's built-in functions that have no prototype can be
    generated from this command:
    
      $ perldoc -m perlfunc | perl -lne '
          next unless /Perl Functions by Category/ .. /Portability/;
          if (/[IC]</) {
            push @f, map {
              s/\s+//;
              s/[IC]<//g;
              s/>//g;
              s{[^a-zA-Z0-9_\-/]}{}g;
              $_
            }
            grep { /[IC]</ }
            map { split / /, $_ }
            split /,/, $_;
          }
    
          END {
            %s;
            print foreach
              grep { ! $s{$_}++ && ! defined eval { CORE::prototype("CORE::$_") } }
              map { s,/,,g; $_ }
              sort {
                ($b =~ m{/}) <=> ($a =~ m{/}) ||
                $a cmp $b
              } @f;
          }
      '
    
    For perl v5.10.1, this list is:
    
        m//, q//, qq//, qr//, qw//, qx//, s///, tr///, y///, -X,
        chomp, chop, default, defined, delete, do, eval, exec,
        exists, format, given, glob, goto, grep, import, last,
        local, map, my, next, no, our, package, pos, print,
        printf, prototype, redo, require, return, say, scalar,
        sort, split, state, study, sub, system, tie, tied, undef,
        untie, use, when
    
    However, this list is incomplete ('if' doesn't appear there, because it
    is not a core function; it is a keyword) and it is inaccurate ('glob'
    is well-known to be overridable despite having no prototype).
    
    Maybe in another commit I'll extract all the toke.c:Perl_keywords() from
    each version of Perl in order to allow this module to truly do what it's
    intending to do by checking the definedness of the prototype.

--- lib/universal/overridable.pm
+++ lib/universal/overridable.pm
@@ -1,22 +1,68 @@
 package universal::overridable;
+use strict;
+use warnings;
+
+use Carp ();
 
 sub import {
   my ($class, @funcs) = @_;
+  $class->make_global_stubs(@funcs);
+}
+
+sub make_global_stubs {
+  my ($class, @funcs) = @_;
+
   $class->make_global_stub($_) foreach @funcs;
 }
 
 sub make_global_stub {
   my ($class, $func) = @_;
 
-  my ($sub) = $func =~ m{^.*:(\w+)$};
+  my ($package, $sub) = $func =~ m{^(.*)::(\w+)$};
   my $proto = CORE::prototype($func);
-  my $prototype = defined $proto ? "($proto)" : "";
-  my $pass_args = $proto ? "\@_" : "";
+  if (! defined($proto) && $package eq 'CORE') {
+    $class->confess_from_perspective_of_using_module("$func has no prototype so is not overridable, please rethink your approach");
+  }
 
-  eval <<"EVAL";
-    sub CORE::MONKEYPATCH::$sub $prototype { return CORE::$sub($pass_args) }
-    *CORE::GLOBAL::$sub = sub $prototype { goto &CORE::MONKEYPATCH::$sub };
-EVAL
+  my ($prototype, $pass_args) = $proto
+    ? ("($proto)", (defined $proto ? '\@_' : ''))
+    : ("", '');
+
+  if ($package eq 'CORE') {
+    eval <<"    EVAL";
+      sub CORE::MONKEYPATCH::$sub $prototype { return CORE::$sub($pass_args) }
+      *CORE::GLOBAL::$sub = sub $prototype { goto &CORE::MONKEYPATCH::$sub };
+    EVAL
+    die $@ if $@;
+  } else {
+    my $o = do {
+      no strict 'refs';
+      *{${"$package\::"}{$sub}}{CODE};
+    };
+    eval <<"    EVAL";
+      no warnings 'redefine';
+      *$package\::MONKEYPATCH::$sub = \$o;
+      *$package\::$sub = sub $prototype { goto &$package\::MONKEYPATCH::$sub };
+    EVAL
+    die $@ if $@;
+  }
+}
+
+sub confess_from_perspective_of_using_module {
+  my ($class, $message) = @_;
+
+  # Run back up the stack to a point where someone is trying to 'use' us.
+  # (Unfortunately setting @CARP_NOT didn't help here, since some frames we need to
+  # ignore are 'eval' frames, but the one we're looking for is also an 'eval'
+
+  my $lookback = 0;
+  while (my @frame = caller($lookback++)) {
+    next if ! defined $frame[6];
+    last if $frame[6] =~ /$class/;
+  }
+  local $Carp::CarpLevel = $lookback;
+
+  Carp::confess($message);
 }
 
 1;
--- t/monkeypatch.t
+++ t/monkeypatch.t
@@ -2,9 +2,10 @@
 use strict;
 use warnings;
 
-use Test::More tests => 3;
+use Test::More tests => 8;
 
 use FindBin ();
+use File::Spec;
 use lib "$FindBin::Bin/../lib";
 use lib "$FindBin::Bin/lib";
 
@@ -30,4 +31,49 @@ use lib "$FindBin::Bin/lib";
   #   };
   #   cmp_ok( time(), '==', $random_time, 'second monkeypatch time overlaid' );
   # }
+}
+
+# functions with an 'undef' prototype cannot be overridden, let's catch that
+{
+  my $me = File::Spec->join($FindBin::Bin, $FindBin::Script);
+
+  {
+    local $@;
+    eval <<EVAL;
+      use universal::overridable qw(CORE::map);
+EVAL
+    my $error = $@;
+
+    like(
+      $error,
+      qr/CORE::map has no prototype so is not overridable, please rethink your approach at $me line \d+/,
+      'We correctly inform the programmer about non-overridable functions'
+     );
+  }
+
+  {
+    my $return = 'markup-wormed';
+    local $@;
+    eval <<EVAL;
+      BEGIN {
+        sub sixth::bating { return "$return" }
+        use universal::overridable qw(sixth::bating);
+      }
+EVAL
+    my $error = $@;
+
+    is( $error, '', 'no errors trying to override non-core functions without a prototype' );
+    is( sixth->bating, $return, 'we get the proper return value' );
+
+    {
+      no warnings 'redefine';
+      my $revised_return = 4548;
+      local *sixth::MONKEYPATCH::bating = sub { $revised_return };
+      is( sixth->bating, $revised_return, 'we can swap out the function' );
+    }
+
+    # This is really a test of the 'local' in 'local *sixth::MONKEYPATCH::bating = sub { ... }'
+    # but it's reassuring to prove that this works like it should.
+    is( sixth->bating, $return, 'original return value restored' );
+  }
 }
