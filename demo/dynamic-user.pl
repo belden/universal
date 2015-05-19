2534a62802bdf24d140f63566b8c27138aed0f34
Belden Lyman <belden@mediamath.com>
2015-05-19 11:52:43 -0400

    whitespace cleanup

--- demo/dynamic-user.pl
+++ demo/dynamic-user.pl
@@ -7,10 +7,10 @@ use warnings;
   package toy::class;
 
   sub dumper_class { 'Data::Dumper' }
-  
+
   sub go {
     my ($class) = @_;
-  
+
     use universal::dynamic_use;
     print $class->dynamic_use->dumper_class->Dump([qw(hello world)]);
   }
